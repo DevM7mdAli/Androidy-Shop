e868727ecd27339aa0b505cef2a188d4ea1c198e
DevM7mdAli <DevM7mdAli@outlook.com>
2024-02-27 13:21:06 +0300

    init repo

---
+++ android/app/src/main/kotlin/com/example/androidyshop/MainActivity.kt
@@ -0,0 +1,5 @@
+package com.example.androidyshop
+
+import io.flutter.embedding.android.FlutterActivity
+
+class MainActivity: FlutterActivity()
---
+++ lib/constants/constants.dart
@@ -0,0 +1,14 @@
+
+import 'package:flutter/material.dart';
+
+
+//! for putting hex values you need to replace # with 0x
+const cBackGroundColor = Color(0x448AFF);
+const cSecondryBackGroundColor = Color(0xF5F5F5);
+const cPriceBoxColor = Color(0xFFC107);
+const cPrimaryCardBackColor = Color(0xFFFFFFFF);
+const cTextColor = Color(0x212121);
+const cTextSecondryColor = Color(0xBDBDBD);
+
+
+const cDefaultPadding = 20;
